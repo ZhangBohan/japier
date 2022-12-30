acfff42d7584f4538f935fcbc0c9f212331ef969
zhangping <zhangping@hosecloud.com>
2022-12-30 22:51:54 +0800

    :construction: init jacoco

---
+++ src/main/java/com/bohanzhang/japier/Demo.java
@@ -0,0 +1,8 @@
+package com.bohanzhang.japier;
+
+import lombok.Data;
+
+@Data
+public class Demo {
+    private String id;
+}
--- src/test/java/com/bohanzhang/japier/JapierApplicationTests.java
+++ src/test/java/com/bohanzhang/japier/JapierApplicationTests.java
@@ -1,13 +1,17 @@
 package com.bohanzhang.japier;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mvel2.MVEL;
 
+@Slf4j
 class JapierApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Demo demo = new Demo();
+		log.debug("{}", demo);
 		Object eval = MVEL.eval("1 + 1");
 		Assertions.assertEquals(2, eval);
 	}
